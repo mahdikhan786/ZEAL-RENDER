5a2c508066c40047a9d120a596a5f31b0d49197d
khansofficial <khanmahdi786@gmail.com>
2020-01-19 13:20:56 +0530

    Updates

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿
+// getting the Dom elements
+const menuToggle = document.querySelector('.menu-toggle'); // hamburger-menu
+const toggleBody = document.querySelector('.toggle-body'); // menu-bar
+const toggleIcon = document.querySelector('.toggler'); // central-hamburger-line
+
+// toggling the class name for css animation
+const toggle = () => {
+    toggleBody.classList.toggle('display-none');
+    toggleIcon.classList.toggle('rotate');
+}
+
+menuToggle.addEventListener('click', toggle);
